2d03db1f829415ef262361a38981f4c620949afa
sarteidebanda <higor.aguiar.moraes@escola.pr.gov.br>
2024-11-13 09:32:28 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,28 @@
+let cor;
+let circuloX;
+let contador;
+
 function setup() {
   createCanvas(400, 400);
+  background(color(100,0,0));
+  cor =color(random(0,255),random(0,255),random(0,255));
+  circuloX = [0,0,0,0,0];
+  circuloY = [random(height), random(height), random(height), random(height), random(height)];
 }
-
+              
 function draw() {
-  background(220);
+  fill(cor);
+  for (contador in circuloX) {
+    circle(circuloX[contador], circuloY[contador], 50);
+    circuloX[contador] += random(0,3);
+    circuloY[contador] -= random(-3, 3);
+   if (circuloX[contador] >= width) {
+      circuloX[contador] = 0;
+      circuloY[contador] = random(height);
+      
+    }
+  }
+  if (mouseIsPressed) {
+    cor = color(random(0,255), random(0,255), random(0,255), random(0, 100));
+  }
 }
